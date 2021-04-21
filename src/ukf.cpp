4ebf40ba3f5834a3fea07d42d3040bd8e66a2f49
daniele-bruno <daniele.bruno@fcagroup.com>
2021-04-21 18:48:54 +0200

    complete prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -117,7 +117,103 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+  // create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
 
+  // create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  // create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // create augmented mean state
+  x_aug.head(5) =  x_;
+  x_aug.tail(2).fill(0.);
+
+  // create augmented covariance matrix
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(5,5) = P_;
+  P_aug(5,5) = std_a_*std_a_;
+  P_aug(6,6) = std_yawdd_*std_yawdd_;
+
+  // create square root matrix
+  MatrixXd A_aug = P_aug.llt().matrixL();
+
+  // create augmented sigma points
+  // set sigma mean point
+  Xsig_aug.col(0) = x_aug;
+  // set sigma points to the right and left of mean
+  for (unsigned int i=0; i<n_aug_; i++)
+  {
+      Xsig_aug.col(i+1)       = x_aug + sqrt(lambda_ + n_aug_) * A_aug.col(i); // right
+      Xsig_aug.col(i+n_aug_+1) = x_aug - sqrt(lambda_ + n_aug_) * A_aug.col(i); // left
+  }
+  
+  // predict sigma points
+  for (unsigned int i = 0; i<(2*n_aug_+1); i++)
+  {
+    // get state variables and noises from sigma point 
+    double x        = Xsig_aug(0, i);
+    double y        = Xsig_aug(1, i);    
+    double v        = Xsig_aug(2, i);
+    double psi      = Xsig_aug(3, i);
+    double psid     = Xsig_aug(4, i);
+    double nu_a     = Xsig_aug(5, i);
+    double nu_psidd = Xsig_aug(6, i);
+
+    // initialize new state variables
+    double x_n, y_n, v_n, psi_n, psid_n;
+
+    // if yaw rate is not zero
+    if (psid != 0)
+    {
+      // increment x and y using normal formula
+      x_n = x + v / psid * (sin(psi + psid * delta_t) - sin(psi));
+      y_n = y + v / psid * (-cos(psi + psid * delta_t) + cos(psi));
+    }
+    else
+    {
+      // avoid division by zero
+      x_n = v * cos(psi) * delta_t;
+      y_n = v * sin(psi) * delta_t;
+    }
+    // increment remaining state variables
+    v_n    = v;
+    psi_n  = psi + psid * delta_t;
+    psid_n = psid;
+
+    // add noise
+    double  dt2 = delta_t*delta_t;
+    x_n     = x_n + 0.5 * dt2 * cos(psi) * nu_a;
+    y_n     = y_n + 0.5 * dt2 * sin(psi) * nu_a;
+    v_n     = v_n + delta_t * nu_a;
+    psi_n   = psi_n + 0.5 * dt2 * nu_psidd;
+    psid_n  = psid_n + delta_t * nu_psidd;
+
+    // write predicted sigma points into right column
+    Xsig_pred_(0,i) = x_n;
+    Xsig_pred_(1,i) = y_n;
+    Xsig_pred_(2,i) = v_n;
+    Xsig_pred_(3,i) = psi_n;
+    Xsig_pred_(4,i) = psid_n;
+  }
+
+  // predict state mean
+  x_ = Xsig_pred_ * weights_;
+
+  // predict state covariance matrix
+  P_.fill(0.0);
+  // iterate over sigma points
+  for (unsigned int i = 0; i < 2*n_aug_+1; i++)
+  {
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+
+    // normalize psi angle to -pi, pi
+    while (x_diff(3) > M_PI)  x_diff(3) -= 2*M_PI;
+    while (x_diff(3) < -M_PI) x_diff(3) += 2*M_PI;
+
+    P_ += weights_(i) * x_diff * x_diff.transpose();
+  }
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
@@ -127,6 +223,7 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
    * covariance, P_.
    * You can also calculate the lidar NIS, if desired.
    */
+
 }
 
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
